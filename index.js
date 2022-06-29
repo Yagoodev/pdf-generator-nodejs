28eb064467ae0df0f496d6518bfb2eb0182a8de3
Fhare <yago.yaguinho@live.com>
2022-06-29 17:03:24 -0300

    Adding new headers config

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const app = express();
 app.use(express.json());
 
 app.get("/api", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000/");
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); 
+  res.setHeader('Access-Control-Allow-Credentials', true);
 
   return res.json(JSON.stringify(api));
 });
