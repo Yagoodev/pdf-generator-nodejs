c6ea109e9036627422f6e6d3d4d98c0341b257f2
Fhare <yago.yaguinho@live.com>
2022-06-29 17:10:09 -0300

    Remove headers

--- index.js
+++ index.js
@@ -13,11 +13,6 @@ app.use(express.json());
 app.use(cors(corsOptions));
 
 app.get("/api", (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); 
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
   return res.json(JSON.stringify(api));
 });
 
