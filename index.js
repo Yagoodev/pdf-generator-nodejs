c98e03d3b9f0d3ad12b4751a4cbe5de228cf8303
Fhare <yago.yaguinho@live.com>
2022-06-29 16:43:25 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const api = require("./db");
+const app = express();
+
+app.use(express.json());
+
+app.get("/api", (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  return res.json(JSON.stringify(api));
+});
+
+app.listen(3001);
