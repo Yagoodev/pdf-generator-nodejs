9de0bc2669f1dc052dea4ca610f0e76b915ecbab
Fhare <yago.yaguinho@live.com>
2022-06-29 17:07:45 -0300

    Cors config

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const api = require("./db");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/api", (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/');
